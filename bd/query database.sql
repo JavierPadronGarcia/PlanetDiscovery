6673327dcdf689865d6ffd522d3658054ee0a525
JavierPadronGarcia <padrongarciajavier04@gmail.com>
2023-10-06 13:39:39 +0100

    changed the sql query create db and tables

--- bd/query database.sql
+++ bd/query database.sql
@@ -1,18 +1,22 @@
-create database if not exists planets;
-use planets;
+CREATE DATABASE `planets`;
+USE `planets`;
 
-create table planet(
-id int not null auto_increment primary key,
-name varchar(50),
-composition varchar(200)
+CREATE TABLE `planet` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `composition` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
-create table satellite(
-id int not null auto_increment primary key,
-name varchar(50),
-composition varchar(200),
-planet_id int,
-constraint sat_fk foreign key (planet_id) references planet(id) on delete cascade on update cascade);
+CREATE TABLE `satellite` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `composition` varchar(200) DEFAULT NULL,
+  `planet_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sat_fk_idx` (`planet_id`),
+  CONSTRAINT `sat_fk` FOREIGN KEY (`planet_id`) REFERENCES `planet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 insert into planet (name, composition) values ('Tierra', 'Rocoso');
 insert into satellite(name, composition, planet_id) values ('Luna', 'Rocoso', 1);
