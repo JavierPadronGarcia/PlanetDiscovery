2a77ee417da086041f5d493dadcd2fe2a9b9e17e
JavierPadronGarcia <padrongarciajavier04@gmail.com>
2023-10-17 16:24:22 +0100

    deleted the backend to change to express

--- backend/src/main/java/com/javier/backend/entity/dao/IPlanetDao.java
+++
@@ -1,8 +0,0 @@
-package com.javier.backend.entity.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.javier.backend.entity.model.Planet;
-
-public interface IPlanetDao extends CrudRepository<Planet, Long > {
-
-}
